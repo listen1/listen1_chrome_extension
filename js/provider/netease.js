9c61cd3bf3de73a11ba42b6f5a6fbc0aeaeb0f21
Junhan <junhanoct@gmail.com>
2016-05-26 20:30:00 -0700

    use map to map source id and source name; some clean work.

--- js/provider/netease.js
+++ js/provider/netease.js
@@ -3,8 +3,8 @@ var netease = (function() {
 
     var ne_show_playlist = function(url, hm) {
 
-        var order = "hot"
-        var offset = getParameterByName('offset',url)
+        var order = "hot";
+        var offset = getParameterByName('offset', url);
 
         if (offset !=  null) {
             var target_url = 'http://music.163.com/discover/playlist/?order=' + order + '&limit=35&offset=' + offset;
@@ -27,7 +27,7 @@ var netease = (function() {
                         default_playlist.cover_img_url = $(this).find('img')[0].src;
                         default_playlist.title = $(this).find('div a')[0].title;
                         var url = $(this).find('div a')[0].href;
-                        var list_id = getParameterByName('id',url);
+                        var list_id = getParameterByName('id', url);
                         default_playlist.id = 'neplaylist_' + list_id;
                         default_playlist.source_url = 'http://music.163.com/#/playlist?id=' + list_id;
                         result.push(default_playlist);
@@ -157,9 +157,9 @@ var netease = (function() {
     }
 
     function _encrypted_request(text) {
-        var modulus = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b72' + 
-        '5152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbd' + 
-        'a92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe48' + 
+        var modulus = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b72' +
+        '5152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbd' +
+        'a92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe48' +
         '75d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7';
         var nonce = '0CoJUm6Qyw8W8jud';
         var pubKey = '010001';
@@ -211,7 +211,7 @@ var netease = (function() {
     function is_playable(song) {
         return ((song.status >= 0) && (song.fee != 4));
     }
-    
+
     var ne_search = function(url, hm, se) {
         // use chrome extension to modify referer.
         var target_url = 'http://music.163.com/api/search/pc';
