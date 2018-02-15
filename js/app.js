fdf003e7a9159f2613449c0fdd6e5814f94c9ff4
Sibo Jia <evergreen132@gmail.com>
2018-02-15 10:48:39 +0800

    change symbol in page title according to playing status (#105)

--- js/app.js
+++ js/app.js
@@ -825,6 +825,12 @@
       });
 
       $scope.$on('music:isPlaying', function(event, data) {
+        if (data) {
+          $rootScope.page_title = '▶' + $rootScope.page_title.slice(1);
+        }
+        else {
+          $rootScope.page_title = '⏸' + $rootScope.page_title.slice(1);
+        };
         if (!lastfm.isAuthorized()) {
           return;
         }
