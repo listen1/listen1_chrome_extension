13576431dad452850db423acbe5081bc343253b3
Xiaochen Wang <ryan.wang23@hotmail.com>
2018-10-12 01:28:20 -0400

    first commit

---
+++ js/translate.js
@@ -0,0 +1,23 @@
+// var arrLang = {
+//     'en': {
+//         'songList' : 'Song List',
+//         'myList' : 'My Song List',
+//         'search' : 'Search',
+//         'about' : 'About'
+//     },
+//     'cn': {
+//         'songList' : '精选歌单',
+//         'myList' : '我的歌单',
+//         'search' : '快速搜索',
+//         'about' : '关于'
+//     }
+// }
+// $(()=>{
+//     $('.translate').click(()=>{
+//         var lang = $(this).attr('id');
+
+//         $('.lang').each(function(index,element){
+//             $('this').text(arrLang[lang][$(this).attr('key')])
+//         });
+//     });
+// });
