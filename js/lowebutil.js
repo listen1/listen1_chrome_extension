7d4e5f1f5af1b4f675405e00df9922d1d744cf0a
Alphanow <alphanow@gmail.com>
2019-01-07 11:03:05 +0800

    Fix eslint issues in backgroud.js and lowebutil.js

--- js/lowebutil.js
+++ js/lowebutil.js
@@ -1,9 +1,12 @@
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+/* global window  */
+/* eslint-disable no-param-reassign */
+export default function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&'); // eslint-disable-line no-useless-escape
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
