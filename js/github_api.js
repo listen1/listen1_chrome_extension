cc20ac994c3a2f9c0edbf47e16c4fb25ccf7ffc9
Alphanow <alphanow@gmail.com>
2019-01-08 13:38:38 +0800

    Fix eslint issues in github.js

--- js/github_api.js
+++ js/github_api.js
@@ -1,120 +1,113 @@
-(function() {
-  var OAUTH_URL = 'https://github.com/login/oauth';
-  var API_URL = 'https://api.github.com';
+/* global Storage $ localStorage window */
+function github() {
+  const OAUTH_URL = 'https://github.com/login/oauth';
+  const API_URL = 'https://api.github.com';
 
-  var client_id = 'e099a4803bb1e2e773a3';
-  var client_secret = '81fbfc45c65af8c0fbf2b4dae6f23f22e656cfb8';
+  const client_id = 'e099a4803bb1e2e773a3';
+  const client_secret = '81fbfc45c65af8c0fbf2b4dae6f23f22e656cfb8';
 
-  Storage.prototype.setObject = function(key, value) {
-      this.setItem(key, JSON.stringify(value));
-  }
+  Storage.prototype.setObject = (key, value) => {
+    this.setItem(key, JSON.stringify(value));
+  };
 
-  Storage.prototype.getObject = function(key) {
-      var value = this.getItem(key);
-      return value && JSON.parse(value);
-  }
+  Storage.prototype.getObject = (key) => {
+    const value = this.getItem(key);
+    return value && JSON.parse(value);
+  };
 
-  var Github = {
+  const Github = {
     status: 0,
     username: '',
-    getOAuthUrl: function(){
-      this.status = 1;
-      return OAUTH_URL + '/authorize?client_id=' + client_id  + '&scope=gist';
+    getOAuthUrl: () => {
+      Github.status = 1;
+      return `${OAUTH_URL}/authorize?client_id=${client_id}&scope=gist`;
     },
 
-    updateStatus: function(callback){
-      var access_token = localStorage.getObject('githubOauthAccessKey');
+    updateStatus: (callback) => {
+      const access_token = localStorage.getObject('githubOauthAccessKey');
       if (access_token == null) {
-        this.status = 0;
+        Github.status = 0;
         return;
       }
-      var self = this;
-      this.api('/user', function(data){
-        if (data.login == undefined) {
+      const self = Github;
+      Github.api('/user', (data) => {
+        if (data.login === undefined) {
           self.status = 1;
-        }
-        else {
+        } else {
           self.status = 2;
           self.username = data.login;
         }
-        if(callback != null) {
+        if (callback != null) {
           callback(self.status);
         }
       });
     },
 
-    getStatus: function(){
-      return this.status;
+    getStatus: () => Github.status,
+
+    getStatusText: () => {
+      switch (Github.status) {
+        case 0:
+          return '未连接';
+        case 1:
+          return '连接中';
+        case 2:
+          return `${Github.username}已登录`;
+        default:
+          return '???';
+      }
     },
 
-    getStatusText: function(){
-      if(this.status == 0) {
-        return '未连接';
-      }
-      if(this.status == 1) {
-        return '连接中';
-      }
-      if(this.status == 2) {
-        return this.username + '已登录';
-      }
-      return '???'
+    setStatus: (newStatus) => {
+      Github.status = newStatus;
     },
 
-    setStatus: function(newStatus){
-      this.status = newStatus;
-    },
-
-
-    handleCallback: function(code, cb){
-      var url = OAUTH_URL + '/access_token';
-      var data = {
-        client_id: client_id,
-        client_secret: client_secret,
-        code: code
+    handleCallback: (code, cb) => {
+      const url = `${OAUTH_URL}/access_token`;
+      const data = {
+        client_id,
+        client_secret,
+        code,
       };
       $.ajax({
-        url: url,
+        url,
         headers: {
-          Accept: 'application/json'
+          Accept: 'application/json',
         },
-        dataType: "json",
-        data: data,
-        success: function(response) {
-          var ak = response.access_token;
+        dataType: 'json',
+        data,
+        success: (response) => {
+          const ak = response.access_token;
           localStorage.setObject('githubOauthAccessKey', ak);
-          if(cb != undefined) {
+          if (cb !== undefined) {
             cb(ak);
           }
-        }
+        },
       });
     },
 
-    api: function(apiPath, cb){
-      var access_token = localStorage.getObject('githubOauthAccessKey') || '';
-      var url = API_URL + apiPath + '?access_token=' + access_token;
-      $.get(url, function(response){
+    api: (apiPath, cb) => {
+      const access_token = localStorage.getObject('githubOauthAccessKey') || '';
+      const url = `${API_URL}${apiPath}?access_token=${access_token}`;
+      $.get(url, (response) => {
         cb(response);
-      })
-    },
-
-    logout: function() {
-      localStorage.removeItem('githubOauthAccessKey');
-      this.status = 0;
-    },
-
-    isLoggedIn: function() {
-      return localStorage.getObject('githubOauthAccessKey') != null;
-    },
-
-    deparam: function(params) {
-      var obj = {};
-      $.each(params.split('&'), function() {
-        var item = this.split('=');
-        obj[item[0]] = item[1];
       });
-      return obj;
-    }
+    },
+
+    logout: () => {
+      localStorage.removeItem('githubOauthAccessKey');
+      Github.status = 0;
+    },
+
+    isLoggedIn: () => localStorage.getObject('githubOauthAccessKey') !== null,
+
+    deparam: params => (new URLSearchParams(params).keys).reduce((r, keys) => {
+      r[keys] = params[keys]; // eslint-disable-line no-param-reassign
+      return r;
+    }, {}),
   };
 
   window.Github = Github;
-})();
+}
+
+github();
