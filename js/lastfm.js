9518fd737a28210e663cb484a4464d81756c7d76
Alphanow <alphanow@gmail.com>
2019-01-08 12:12:04 +0800

    Fix eslint issues in lastfm.js

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,33 +1,31 @@
-(function() {
+/* global Storage localStorage */
+/* global angular MD5 */
+function lastfm() {
+  Storage.prototype.setObject = (key, value) => {
+    this.setItem(key, JSON.stringify(value));
+  };
 
- 'use strict';
+  Storage.prototype.getObject = (key) => {
+    const value = this.getItem(key);
+    return value && JSON.parse(value);
+  };
 
-  Storage.prototype.setObject = function(key, value) {
-      this.setItem(key, JSON.stringify(value));
-  }
-
-  Storage.prototype.getObject = function(key) {
-      var value = this.getItem(key);
-      return value && JSON.parse(value);
-  }
-
-  angular.module('lastfmClient', []).provider('lastfm', function() {
+  angular.module('lastfmClient', []).provider('lastfm', function lastfm_func() {
     this.options = {
       apiKey: 'unknown',
-      apiSecret: 'unknown'
+      apiSecret: 'unknown',
     };
 
-    this.setOptions = function(options) {
-      if (!angular.isObject(options)) throw new Error("Options should be an object!");
+    this.setOptions = (options) => {
+      if (!angular.isObject(options)) throw new Error('Options should be an object!');
       this.options = angular.extend({}, this.options, options);
     };
 
     this.apiUrl = 'https://ws.audioscrobbler.com/2.0/';
 
-    this.$get = ['$http', '$window', function($http, $window) {
-      var options = this.options;
-      var apiUrl = this.apiUrl;
-      var status = 0;
+    this.$get = ['$http', '$window', ($http, $window) => {
+      const { options, apiUrl } = this;
+      let status = 0;
 
       /**
        * Computes string for signing request
@@ -35,25 +33,12 @@
        * See http://www.last.fm/api/authspec#8
        */
       function generateSign(params) {
-        var keys = [];
-        var o = '';
-
-        for (var x in params) {
-          if (params.hasOwnProperty(x)) {
-            keys.push(x);
-          }
-        }
+        const keys = Object.keys(params).filter(key => (key !== 'format' || key !== 'callback'));
 
         // params has to be ordered alphabetically
         keys.sort();
 
-        for (var i = 0; i < keys.length; i++) {
-          if (keys[i] == 'format' || keys[i] == 'callback') {
-            continue;
-          }
-
-          o = o + keys[i] + params[keys[i]];
-        }
+        const o = keys.reduce((r, key) => (r + key + params[key]), '');
 
         // append secret
         return MD5(o + options.apiSecret);
@@ -63,41 +48,70 @@
        * Creates query string from object properties
        */
       function createQueryString(params) {
-        var parts = [];
-
-        for (var x in params) {
-          if (params.hasOwnProperty(x)) {
-            parts.push( x + '=' + encodeURIComponent(params[x]));
-          }
-        }
-
+        const parts = [];
+        Object.keys(params).forEach(key => parts.push(`${key}=${encodeURIComponent(params[key])}`));
         return parts.join('&');
       }
 
-      function getAuth(callback){
-        var url = apiUrl + '?method=auth.gettoken&api_key=' + options.apiKey  + '&format=json';
-        $http.get(url).then(function(response) {
-          var data = response.data;
-          var token = data.token;
-          localStorage.setObject('lastfmtoken', token);
-          var grant_url = 'http://www.last.fm/api/auth/?api_key=' + options.apiKey + '&token=' + token;
-          $window.open(grant_url, '_blank');
-          status = 1;
-          if (callback != null) {
-            callback();
+      function _isAuthRequested() { // eslint-disable-line no-underscore-dangle
+        const token = localStorage.getObject('lastfmtoken');
+        return (token != null);
+      }
+
+      function getSession(callback) {
+        // load session info from localStorage
+        let mySession = localStorage.getObject('lastfmsession');
+        if (mySession != null) {
+          return callback(mySession);
+        }
+        // trade session with token
+        const token = localStorage.getObject('lastfmtoken');
+        if (token == null) {
+          return callback(null);
+        }
+        // token exists
+        const params = {
+          method: 'auth.getsession',
+          api_key: options.apiKey,
+          token,
+        };
+        const apiSig = generateSign(params);
+        const url = `${apiUrl}?${createQueryString(params)}&api_sig=${apiSig}&format=json`;
+        $http.get(url).then((response) => {
+          const { data } = response;
+          mySession = data.session;
+          localStorage.setObject('lastfmsession', mySession);
+          callback(mySession);
+        }).error((errResponse, statusCode) => {
+          if (statusCode === 403) {
+            callback(null);
           }
         });
-      };
+        return null;
+      }
 
-      function cancelAuth(){
-        localStorage.removeItem('lastfmsession');
-        localStorage.removeItem('lastfmtoken');
-        updateStatus();
-      };
+      function getUserInfo(callback) {
+        getSession((session) => {
+          if (session == null) {
+            callback(null);
+            return;
+          }
+          const params = {
+            method: 'user.getinfo',
+            api_key: options.apiKey,
+            sk: session.key,
+          };
 
-      function _isAuthRequested() {
-        var token = localStorage.getObject('lastfmtoken');
-        return (token != null);
+          params.api_sig = generateSign(params);
+
+          const url = `${apiUrl}?${createQueryString(params)}&format=json`;
+          $http.post(url).then((response) => {
+            const { data } = response;
+            if (callback != null) {
+              callback(data);
+            }
+          });
+        });
       }
 
       function updateStatus() {
@@ -109,155 +123,122 @@
           status = 0;
           return;
         }
-        getUserInfo(function(data){
-          if (data == null) {
+        getUserInfo((data) => {
+          if (data === null) {
             status = 1;
-          }
-          else {
+          } else {
             status = 2;
           }
         });
       }
 
-      function getSession(callback) {
-        // load session info from localStorage
-        var mySession = localStorage.getObject('lastfmsession');
-        if (mySession != null) {
-          return callback(mySession);
-        }
-        // trade session with token
-        var token = localStorage.getObject('lastfmtoken');
-        if (token == null){
-          return callback(null);
-        }
-        // token exists
-        var params = {
-          method: 'auth.getsession',
-          api_key: options.apiKey,
-          token: token
-        };
-        var apiSig = generateSign(params);
-        var url = apiUrl + '?' + createQueryString(params) + '&api_sig=' + apiSig + '&format=json';
-        $http.get(url).then(function(response){
-          var data = response.data;
-          mySession = data.session;
-          localStorage.setObject('lastfmsession', mySession);
-          callback(mySession);
-        }).error(function (errResponse, status) {
-          if(status == 403){
-            callback(null);
+      function getAuth(callback) {
+        const url = `${apiUrl}?method=auth.gettoken&api_key=${options.apiKey}&format=json`;
+        $http.get(url).then((response) => {
+          const { data } = response;
+          const { token } = data;
+          localStorage.setObject('lastfmtoken', token);
+          const grant_url = `http://www.last.fm/api/auth/?api_key=${options.apiKey}&token=${token}`;
+          $window.open(grant_url, '_blank');
+          status = 1;
+          if (callback != null) {
+            callback();
           }
         });
-      };
+      }
+
+      function cancelAuth() {
+        localStorage.removeItem('lastfmsession');
+        localStorage.removeItem('lastfmtoken');
+        updateStatus();
+      }
 
       function sendNowPlaying(track, artist, callback) {
-        getSession(function(session){
-          var params = {
+        getSession((session) => {
+          const params = {
             method: 'track.updatenowplaying',
-            track: track,
-            artist: artist,
+            track,
+            artist,
             api_key: options.apiKey,
-            sk: session.key
+            sk: session.key,
           };
 
           params.api_sig = generateSign(params);
 
-          var url = apiUrl + '?' + createQueryString(params) + '&format=json';
-          $http.post(url).then(function(response){
-            var data = response.data;
+          const url = `${apiUrl}?${createQueryString(params)}&format=json`;
+          $http.post(url).then((response) => {
+            const { data } = response;
             if (callback != null) {
               callback(data);
             }
           });
         });
-      };
+      }
 
       function scrobble(timestamp, track, artist, album, callback) {
-        getSession(function(session){
-          var params = {
+        getSession((session) => {
+          const params = {
             method: 'track.scrobble',
             'timestamp[0]': timestamp,
             'track[0]': track,
             'artist[0]': artist,
             api_key: options.apiKey,
-            sk: session.key
+            sk: session.key,
           };
 
-          if ((album !='') && (album != null)) {
+          if ((album !== '') && (album != null)) {
             params['album[0]'] = album;
           }
 
           params.api_sig = generateSign(params);
 
-          var url = apiUrl + '?' + createQueryString(params) + '&format=json';
-          $http.post(url).then(function(response){
-            var data = response.data;
+          const url = `${apiUrl}?${createQueryString(params)}&format=json`;
+          $http.post(url).then((response) => {
+            const { data } = response;
             if (callback != null) {
               callback(data);
             }
           });
         });
-      };
-
-      function getUserInfo(callback) {
-        getSession(function(session){
-          if (session == null) {
-            callback(null);
-            return;
-          }
-          var params = {
-            method: 'user.getinfo',
-            api_key: options.apiKey,
-            sk: session.key
-          };
-
-          params.api_sig = generateSign(params);
-
-          var url = apiUrl + '?' + createQueryString(params) + '&format=json';
-          $http.post(url).then(function(response){
-            var data = response.data;
-            if (callback != null) {
-              callback(data);
-            }
-          });
-        });
-      };
+      }
 
       function isAuthorized() {
-        return (status == 2);
+        return (status === 2);
       }
 
       function isAuthRequested() {
-        return !(status == 0);
+        return !(status === 0);
       }
 
       function getStatusText() {
-        if(status == 0) {
-          return '未连接';
-        }
-        if(status == 1) {
-          return '连接中';
-        }
-        if(status == 2) {
-          return '已连接';
+        switch (status) {
+          case 0:
+            return '未连接';
+          case 1:
+            return '连接中';
+          case 2:
+            return '已连接';
+          default:
+            return '';
         }
       }
 
-      var publicApi = {
-        getAuth               : getAuth,
-        cancelAuth            : cancelAuth,
-        getSession            : getSession,
-        sendNowPlaying        : sendNowPlaying,
-        scrobble              : scrobble,
-        getUserInfo           : getUserInfo,
-        getStatusText         : getStatusText,
-        updateStatus          : updateStatus,
-        isAuthorized          : isAuthorized,
-        isAuthRequested       : isAuthRequested
+      const publicApi = {
+        getAuth,
+        cancelAuth,
+        getSession,
+        sendNowPlaying,
+        scrobble,
+        getUserInfo,
+        getStatusText,
+        updateStatus,
+        isAuthorized,
+        isAuthRequested,
       };
 
       return publicApi;
     }];
   });
-}) ();
+}
 
+lastfm();
