a7463632423a2062de7fb1ecf058b8f289d331de
Listen 1 <githublisten1@gmail.com>
2021-03-29 14:59:07 +0800

    fix: kugou some track not available bug

--- js/provider/kugou.js
+++ js/provider/kugou.js
@@ -105,22 +105,20 @@ function build_kugou() {
   }
 
   function kg_render_playlist_result_item(index, item, params, callback) {
-    let target_url = `${'http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash='}${
-      item.hash
-    }`;
+    const { hash } = item;
 
+    let target_url = `${'http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash='}${hash}`;
     const track = {
-      id: `kgtrack_${item.hash}`,
+      id: `kgtrack_${hash}`,
       title: '',
       artist: '',
       artist_id: '',
       album: '',
       album_id: `kgalbum_${item.album_id}`,
       source: 'kugou',
-      source_url: `http://www.kugou.com/song/#hash=${item.hash}&album_id=${item.album_id}`,
+      source_url: `http://www.kugou.com/song/#hash=${hash}&album_id=${item.album_id}`,
       img_url: '',
-      // url: `xmtrack_${item.hash}`,
-      lyric_url: item.hash,
+      lyric_url: hash,
     };
     // Fix song info
     axios.get(target_url).then((response) => {
@@ -281,18 +279,19 @@ function build_kugou() {
   }
 
   function kg_bootstrap_track(track, success, failure) {
-    const sound = {};
-    const song_id = track.id.slice('kgtrack_'.length);
-    const target_url = `http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash=${song_id}`;
+    const track_id = track.id.slice('kgtrack_'.length);
+    const album_id = track.album_id.slice('kgalbum_'.length);
+    const target_url = `https://wwwapi.kugou.com/yy/index.php?r=play/getdata&callback=jQuery&hash=${track_id}&dfid=dfid&mid=mid&platid=4&album_id=${album_id}`;
 
     axios.get(target_url).then((response) => {
       const { data } = response;
-      if (data.url !== '') {
-        sound.url = data.url; // eslint-disable-line no-param-reassign
-        success(sound);
-      } else {
-        failure(sound);
-      }
+      const jsonString = data.slice('jQuery('.length, data.length - 1 - 1);
+      const info = JSON.parse(jsonString);
+
+      return success({
+        url: info.data.play_url,
+        bitrate: `${info.data.bitrate}kbps`,
+      });
     });
   }
 
