c3ab2a1b160ebbbb00dbb8765e71d524b985b6a2
Listen 1 <githublisten1@gmail.com>
2021-04-21 21:54:44 +0800

    feat: add proxy setting for desktop version

--- js/controller/profile.js
+++ js/controller/profile.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable global-require */
+/* eslint-disable no-undef */
 /* eslint-disable no-param-reassign */
 /* global angular i18next sourceList platformSourceList */
 angular.module('listenone').controller('ProfileController', [
@@ -12,12 +15,69 @@ angular.module('listenone').controller('ProfileController', [
       defaultLang = localStorage.getObject('language');
     }
     $scope.lastestVersion = '';
+
+    $scope.proxyModes = [
+      { name: 'system', displayId: '_PROXY_SYSTEM' },
+      { name: 'direct', displayId: '_PROXY_DIRECT' },
+      { name: 'custom', displayId: '_PROXY_CUSTOM' },
+    ];
+    [$scope.proxyMode] = $scope.proxyModes;
+    $scope.proxyRules = '';
+
+    $scope.modifyProxyRules = () => {
+      if (isElectron()) {
+        const message = 'update_proxy_config';
+        $scope.proxyRules = document.getElementById('proxy-rules').value;
+        const { ipcRenderer } = require('electron');
+        ipcRenderer.send('control', message, {
+          proxyRules: $scope.proxyRules,
+        });
+      }
+    };
+    $scope.changeProxyMode = (option) => {
+      const mode = option.name;
+      if (isElectron()) {
+        if (mode === 'system' || mode === 'direct') {
+          const message = 'update_proxy_config';
+          const { ipcRenderer } = require('electron');
+          ipcRenderer.send('control', message, { mode });
+        } else {
+          $scope.proxyRules = '';
+        }
+      }
+    };
+
     $scope.initProfile = () => {
       const url = `https://api.github.com/repos/listen1/listen1_chrome_extension/releases/latest`;
       axios.get(url).then((response) => {
         $scope.lastestVersion = response.data.tag_name;
       });
+      if (isElectron()) {
+        // proxy config
+        const message = 'get_proxy_config';
+        const { ipcRenderer } = require('electron');
+        ipcRenderer.send('control', message);
+      }
     };
+
+    if (isElectron()) {
+      const { ipcRenderer } = require('electron');
+
+      ipcRenderer.on('proxyConfig', (event, config) => {
+        // parse config
+        if (config.mode === 'system' || config.mode === 'direct') {
+          [$scope.proxyMode] = $scope.proxyModes.filter(
+            (i) => i.name === config.mode
+          );
+          $scope.proxyRules = '';
+        } else {
+          [$scope.proxyMode] = $scope.proxyModes.filter(
+            (i) => i.name === 'custom'
+          );
+          $scope.proxyRules = config.proxyRules;
+        }
+      });
+    }
     $scope.setLang = (langKey) => {
       // You can change the language during runtime
       i18next.changeLanguage(langKey).then((t) => {
@@ -29,6 +89,9 @@ angular.module('listenone').controller('ProfileController', [
             item.displayText = t(item.displayId);
           });
           platformSourceList.forEach((item) => {
+            item.displayText = t(item.displayId);
+          });
+          $scope.proxyModes.forEach((item) => {
             item.displayText = t(item.displayId);
           });
         });
