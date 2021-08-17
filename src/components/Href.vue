50c0256accee86a211d06ff823e933a7202af0e4
Xiaokai Li <mike.lxk@gmail.com>
2021-08-18 01:30:34 +0800

    fix: external link not work refactor: read version from package.json

---
+++ src/components/Href.vue
@@ -0,0 +1,10 @@
+<template>
+    <a :href="props.to" target="_blank" rel="noreferrer">{{ displayName }}</a>
+</template>
+<script setup>
+const props = defineProps({
+    to: String,
+    display: String
+})
+const displayName = props.display || props.to;
+</script>
