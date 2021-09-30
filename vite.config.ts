cbcc74cd87d0beb725b7693bd7f57faab87fb137
Dumeng <alphanow@gmail.com>
2021-09-30 13:14:16 +0800

    add vite for electron

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,29 @@ import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import vue from '@vitejs/plugin-vue';
 import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension';
 import zip from 'rollup-plugin-zip';
-import { defineConfig } from 'vite';
-const { NODE_ENV } = process.env;
+import { defineConfig, build } from 'vite';
+
+const { NODE_ENV, BUILD_ELECTRON } = process.env;
 const production = NODE_ENV === 'production';
-// console.log(Object.keys(process));
-if (process.argv.includes('-w') || process.argv.includes('--watch')) process.env.ROLLUP_WATCH = true;
+if (process.argv.includes('-w') || process.argv.includes('--watch')) process.env.ROLLUP_WATCH = 'true';
+
+const chromeExtPlugin = chromeExtension();
+
+if (BUILD_ELECTRON) {
+  build({
+    configFile: 'vite-electron.config.ts',
+  });
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
-    }
+    },
   },
+  base: '',
+  root: 'src/',
   define: {
     __VUE_I18N_FULL_INSTALL__: 'false',
     __VUE_I18N_LEGACY_API__: 'false',
@@ -24,9 +34,18 @@ export default defineConfig({
     __VUE_PROD_DEVTOOLS__: 'false'
   },
   plugins: [
-    chromeExtension(),
+    {
+      name: 'inject-css',
+      buildStart() {
+        const $ = chromeExtPlugin._plugins.html.cache.html$[0];
+        $('head').last().append($('<link rel="stylesheet" href="assets/main.css">'));
+      },
+    },
+    // @ts-ignore: Type Mismatch Error
+    chromeExtPlugin,
     vueI18n({ include: 'src/i18n/*.json', runtimeOnly: true }),
     vue({ refTransform: true }),
+    // @ts-ignore: Type Mismatch Error
     simpleReloader(),
     production && zip({ dir: 'artifacts' })
   ],
@@ -35,9 +54,10 @@ export default defineConfig({
     minify: false,
     emptyOutDir: true,
     rollupOptions: {
+      external: ['src/assets/main.css'],
       input: ['src/manifest.ts'],
       output: {
-        dir: 'dist',
+        dir: BUILD_ELECTRON ? 'electron_dist/dist' : 'dist',
         format: 'esm',
         entryFileNames: `[name].js`,
         chunkFileNames: `[name].js`,
