a70a1c5293588b04464d4114e20c1e156261bd85
Xiaokai Li <mike.lxk@gmail.com>
2021-10-09 21:38:04 -0400

    fix: broken html lang setting

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,16 +1,17 @@
 import { createI18n } from 'vue-i18n';
-import store from '../store';
+import useSettings from '../composition/settings';
 import en_US from './en_US.json';
 import fr_FR from './fr_FR.json';
 import zh_CN from './zh_CN.json';
 import zh_TC from './zh_TC.json';
+const { settings } = useSettings();
 const messages = {
   'en-US': en_US,
   'fr-FR': fr_FR,
   'zh-CN': zh_CN,
   'zh-TC': zh_TC
 };
-
+export type Language = keyof typeof messages;
 const i18n = createI18n({
   legacy: false,
   locale: 'zh-CN', // set locale
@@ -19,10 +20,10 @@ const i18n = createI18n({
   // If you need to specify other options, you can set other options
   // ...
 });
-export const setLocale = (language: string): void => {
+export const setLocale = (language: Language): void => {
   i18n.global.locale.value = language;
-  store.dispatch('settings/setState', { language });
-  const langMap: {[code: string]: string} = {
+  settings.language = language;
+  const langMap: { [code: string]: string } = {
     'zh-CN': 'zh-Hans',
     'zh-TC': 'zh-Hant',
     'en-US': 'en',
