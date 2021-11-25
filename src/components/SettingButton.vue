d1328bbc78f1899ba004b86fd05420a9061b4a07
MikeLxk <mike.lxk@gmail.com>
2021-11-25 01:22:56 -0500

    fix: line height on button inconsistent (#699)
    :up: deps

---
+++ src/components/SettingButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="setting-button" @click="emit('click')">{{ props.text }}</button>
+</template>
+<script setup lang="ts">
+const emit = defineEmits(["click"]);
+const props = defineProps<{
+    text: string
+}>();
+</script>
+<style scoped>
+.setting-button {
+    padding: 5px;
+    background: var(--button-background-color);
+    margin-right: 4px;
+    color: var(--text-default-color);
+    border-radius: 4px;
+    cursor: pointer;
+    line-height: 1.4;
+}
+.setting-button:hover {
+    background: var(--button-hover-background-color);
+}
+</style>
