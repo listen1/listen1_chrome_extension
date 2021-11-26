ef2cb9e64c9a8e1fb9f45b41c7cb6d216f5e98d9
Listen 1 <githublisten1@gmail.com>
2021-11-26 16:35:58 +0800

    fix: migu avatar url error

--- src/provider/migu.js
+++ src/provider/migu.js
@@ -750,7 +750,7 @@ export default class migu extends MusicResource {
         user_id: data.user.uid,
         user_name: data.user.mobile,
         nickname: data.user.nickname,
-        avatar: data.user.avatar.midAvatar,
+        avatar: 'https:' + data.user.avatar.smallAvatar,
         platform: 'migu',
         data
       };
