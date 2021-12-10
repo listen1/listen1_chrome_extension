b0d292c8c15f233e080219d0585ad25f59d6fd9d
Listen 1 <githublisten1@gmail.com>
2021-12-10 17:27:24 +0800

    fix: source tab in search page style error

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,58 +4,26 @@
     <div class="site-wrapper-innerd">
       <div class="cover-container">
         <div class="searchbox">
-          <ul class="source-list">
-            <li class="source-button" :class="{ active: condition.tab === 'allmusic' }" @click="changeSourceTab('allmusic')">
-              <a>{{ t('_ALL_MUSIC') }}(Beta)</a>
-            </li>
-            <div class="splitter" />
-
-            <template v-for="(source, index) in sourceList" :key="source.name">
-              <div class="source-button" :class="{ active: condition.tab === source.name }" @click="changeSourceTab(source.name)">
-                {{ t(source.name) }}
+          <SourceTab :tab="condition.tab" :sources="sourceList" @click="changeSourceTab" :loading="result.loading">
+            <template v-slot:right>
+              <div class="search-type float-right">
+                <li
+                  class="source-button inline-block cursor-pointer border-b hover:border-active hover:text-default"
+                  :class="{ 'border-active': condition.searchType === 0, 'border-transparent text-inactive': condition.searchType !== 0 }"
+                  @click="changeSearchType(0)">
+                  <a>单曲</a>
+                </li>
+                <div class="splitter inline-block mx-3 -mb-0.5 w-px h-4 bg-neutral-400" />
+                <li
+                  class="source-button inline-block cursor-pointer border-b hover:border-active hover:text-default"
+                  :class="{ 'border-active': condition.searchType === 1, 'border-transparent text-inactive': condition.searchType !== 1 }"
+                  @click="changeSearchType(1)">
+                  <a>歌单</a>
+                </li>
               </div>
-              <div v-if="index != sourceList.length - 1" class="splitter" />
             </template>
+          </SourceTab>
 
-            <svg
-              v-show="result.loading"
-              id="loader-1"
-              class="searchspinner"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              x="0px"
-              y="0px"
-              width="40px"
-              height="40px"
-              viewBox="0 0 40 40"
-              enable-background="new 0 0 40 40"
-              xml:space="preserve">
-              <path
-                opacity="0.2"
-                fill="#000"
-                d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z" />
-              <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z">
-                <animateTransform
-                  attributeType="xml"
-                  attributeName="transform"
-                  type="rotate"
-                  from="0 20 20"
-                  to="360 20 20"
-                  dur="0.6s"
-                  repeatCount="indefinite" />
-              </path>
-            </svg>
-            <div class="search-type">
-              <li class="source-button" :class="{ active: condition.searchType === 0 }" @click="changeSearchType(0)">
-                <a>单曲</a>
-              </li>
-              <div class="splitter" />
-              <li class="source-button" :class="{ active: condition.searchType === 1 }" @click="changeSearchType(1)">
-                <a>歌单</a>
-              </li>
-            </div>
-          </ul>
           <ul class="detail-songlist">
             <li v-if="condition.searchType === 0" class="head">
               <div class="title">
@@ -150,6 +118,7 @@ import { useI18n } from 'vue-i18n';
 import useSearch from '../composition/search';
 import { l1Player } from '../services/l1_player';
 import MediaService from '../services/MediaService';
+import SourceTab from '../components/SourceTab.vue';
 
 const showModal = inject('showModal');
 
@@ -174,7 +143,7 @@ const play = (song) => {
 const openUrl = (url) => {
   window.open(url, '_blank').focus();
 };
-const sourceList = computed(() => MediaService.getSourceList());
+const sourceList = computed(() => [{ name: 'allmusic', displayId: '_ALL_MUSIC' }, ...MediaService.getSourceList()]);
 </script>
 
 <style></style>
