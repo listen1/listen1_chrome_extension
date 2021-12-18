a48343bc86b59a715db698089e26009be242463a
Dumeng <alphanow@gmail.com>
2021-12-18 23:04:22 +0800

    Migrate Bili Provider to TS

--- src/provider/bilibili.ts
+++ src/provider/bilibili.ts
@@ -3,7 +3,7 @@ import { getParameterByName } from '../utils';
 import MusicResource from './music_resource';
 
 export default class bilibili extends MusicResource {
-  static bi_convert_song(song_info) {
+  static bi_convert_song(song_info: any) {
     const track = {
       id: `bitrack_${song_info.id}`,
       title: song_info.title,
@@ -18,18 +18,15 @@ export default class bilibili extends MusicResource {
     return track;
   }
 
-  static async showPlaylist(url) {
-    let offset = getParameterByName('offset', url);
-    if (offset === undefined) {
-      offset = 0;
-    }
+  static async showPlaylist(url: string) {
+    const offset = Number(getParameterByName('offset', url) || '0');
     const page = offset / 20 + 1;
     const target_url = `https://www.bilibili.com/audio/music-service-c/web/menu/hit?ps=20&pn=${page}`;
 
     const response = await axios.get(target_url);
     const { data } = response.data.data;
     /** @type {{cover_img_url:string;id:string;source_url:string;title:string}[]}*/
-    const result = data.map((item) => ({
+    const result = data.map((item: any) => ({
       cover_img_url: item.cover,
       title: item.title,
       id: `biplaylist_${item.menuId}`,
@@ -38,8 +35,8 @@ export default class bilibili extends MusicResource {
     return result;
   }
 
-  static async bi_get_playlist(url) {
-    const list_id = getParameterByName('list_id', url).split('_').pop();
+  static async bi_get_playlist(url:string) {
+    const list_id = getParameterByName('list_id', url)?.split('_').pop();
     const target_url = `https://www.bilibili.com/audio/music-service-c/web/menu/info?sid=${list_id}`;
     const target = `https://www.bilibili.com/audio/music-service-c/web/song/of-menu?pn=1&ps=100&sid=${list_id}`;
     const getInfo = async () => {
@@ -53,7 +50,7 @@ export default class bilibili extends MusicResource {
     };
     const getTracks = async () => {
       const { data } = await axios.get(target);
-      const tracks = data.data.data.map((item) => this.bi_convert_song(item));
+      const tracks = data.data.data.map((item: any) => this.bi_convert_song(item));
       return tracks;
     };
     const [info, tracks] = await Promise.all([getInfo(), getTracks()]);
@@ -63,7 +60,7 @@ export default class bilibili extends MusicResource {
     };
   }
 
-  static async bi_album() {
+  static async bi_album(url: string) {
     return {
       tracks: [],
       info: {}
@@ -82,8 +79,8 @@ export default class bilibili extends MusicResource {
     // });
   }
 
-  static async bi_artist(url) {
-    const artist_id = getParameterByName('list_id', url).split('_').pop();
+  static async bi_artist(url: string) {
+    const artist_id = getParameterByName('list_id', url)?.split('_').pop();
     const target_url = `https://api.bilibili.com/x/space/acc/info?mid=${artist_id}&jsonp=jsonp`;
     const target = `https://api.bilibili.com/audio/music-service-c/web/song/upper?pn=1&ps=0&order=2&uid=${artist_id}`;
     const getInfo = async () => {
@@ -97,7 +94,7 @@ export default class bilibili extends MusicResource {
     };
     const getTracks = async () => {
       const { data } = await axios.get(target);
-      return data.data.data.map((item) => this.bi_convert_song(item));
+      return data.data.data.map((item: any) => this.bi_convert_song(item));
     };
     const [tracks, info] = await Promise.all([getTracks(), getInfo()]);
     return {
@@ -106,7 +103,7 @@ export default class bilibili extends MusicResource {
     };
   }
 
-  static async parseUrl(url) {
+  static async parseUrl(url: string) {
     let result;
     const match = /\/\/www.bilibili.com\/audio\/am([0-9]+)/.exec(url);
     if (match != null) {
@@ -119,8 +116,8 @@ export default class bilibili extends MusicResource {
     return result;
   }
 
-  static bootstrapTrack(track, success, failure) {
-    const sound = {};
+  static bootstrapTrack(track: any, success: CallableFunction, failure: CallableFunction) {
+    const sound = {} as any;
     const song_id = track.id.slice('bitrack_'.length);
     const target_url = `https://www.bilibili.com/audio/music-service-c/web/url?sid=${song_id}`;
     axios
@@ -139,8 +136,8 @@ export default class bilibili extends MusicResource {
       .catch(() => failure(sound));
   }
 
-  static async search(url) {
-    const keyword = getParameterByName('keywords', url);
+  static async search(url: string) {
+    const keyword = getParameterByName('keywords', url) || '';
     const au = /\d+$/.exec(keyword);
     if (au != null) {
       const target_url = `https://www.bilibili.com/audio/music-service-c/web/song/info?sid=${au}`;
@@ -170,15 +167,15 @@ export default class bilibili extends MusicResource {
     // return null;
   }
 
-  static async lyric(url) {
+  static async lyric(url: string) {
     // const track_id = getParameterByName('track_id', url).split('_').pop();
-    const lyric_url = getParameterByName('lyric_url', url);
+    const lyric_url = getParameterByName('lyric_url', url) || '';
     const { data } = await axios.get(lyric_url);
     return { lyric: data };
   }
 
-  static getPlaylist(url) {
-    const list_id = getParameterByName('list_id', url).split('_')[0];
+  static getPlaylist(url: string) {
+    const list_id = getParameterByName('list_id', url)?.split('_')[0];
     switch (list_id) {
       case 'biplaylist':
         return this.bi_get_playlist(url);
@@ -210,8 +207,8 @@ export default class bilibili extends MusicResource {
     // empty block
   }
   
-  static async getCommentList(trackId, offset, limit) {
-    const comments = [];
+  static async getCommentList(trackId: string, offset: number, limit: number) {
+    const comments = <any>[];
     return { comments, total: comments.length, offset, limit };
   }
 }
