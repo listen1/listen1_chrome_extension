01835e5f22e78354708100cb667e34fe8decb423
Dumeng <alphanow@gmail.com>
2021-12-28 20:09:17 +0800

    Implement Toggle Button Group

---
+++ src/components/ToggleButtons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="toggle overflow-hidden grid grid-flow-col w-fit divide-x divide-y-0 divide-solid rounded-lg">
+    <SettingButton v-for="(button, index) in options" :key="index" class="rounded-none mr-auto" :text="button.text" @click="emit('change', button.value)" />
+    <!-- <SettingButton text="No" /> -->
+  </div>
+</template>
+<script setup lang="ts">
+import SettingButton from './SettingButton.vue';
+const emit = defineEmits(['change']);
+const { options } = defineProps<{
+  options: {
+    text: string;
+    value?: any;
+  }[];
+}>();
+</script>
+<script lang="ts">
+export default {};
+</script>
+<style>
+.toggle {
+  border-color: var(--text-default-color);
+}
+</style>
