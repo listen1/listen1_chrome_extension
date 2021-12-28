bbbfb33239e0a2d72f0982d35f53613bcf2e702d
Dumeng <alphanow@gmail.com>
2021-12-28 20:35:50 +0800

    Add active indictor

--- src/components/ToggleButtons.vue
+++ src/components/ToggleButtons.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="toggle overflow-hidden grid grid-flow-col w-fit divide-x divide-y-0 divide-solid rounded-lg">
-    <SettingButton v-for="(button, index) in options" :key="index" class="rounded-none mr-auto" :text="button.text" @click="emit('change', button.value)" />
+    <SettingButton
+      v-for="(button, index) in options"
+      :key="index"
+      class="rounded-none mr-auto decoration-2 underline-offset-4"
+      :class="{ underline: selected === index }"
+      :text="button.text"
+      @click="emit('change', button.value)" />
     <!-- <SettingButton text="No" /> -->
   </div>
 </template>
 <script setup lang="ts">
 import SettingButton from './SettingButton.vue';
 const emit = defineEmits(['change']);
-const { options } = defineProps<{
+const { options, selected } = defineProps<{
   options: {
     text: string;
     value?: any;
   }[];
+  selected?: number;
 }>();
 </script>
 <script lang="ts">
@@ -20,5 +27,6 @@ export default {};
 <style>
 .toggle {
   border-color: var(--text-default-color);
+  text-decoration-color: var(--text-default-color);
 }
 </style>
