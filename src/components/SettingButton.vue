6b61550a2953aa44234fcda6b3bf1d5c3db545f3
Dumeng <alphanow@gmail.com>
2021-12-30 13:57:49 +0800

    Adjust toggle button group style

--- src/components/SettingButton.vue
+++ src/components/SettingButton.vue
@@ -1,10 +1,11 @@
 <template>
-  <button class="setting-button p-2 mr-1 rounded cursor-pointer leading-[1.4]" @click="emit('click')">{{ text }}</button>
+  <button class="setting-button p-2 mr-1 rounded cursor-pointer leading-[1.4]" :class="{ invert: selected }" @click="emit('click')">{{ text }}</button>
 </template>
 <script setup lang="ts">
 const emit = defineEmits(['click']);
 const { text } = defineProps<{
   text: string;
+  selected?: boolean;
 }>();
 </script>
 <script lang="ts">
