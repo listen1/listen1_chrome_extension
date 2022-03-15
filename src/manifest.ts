7fd76b2758731e1a3feee93abf669647ec96c1ca
mikelxk <mike.lxk@gmail.com>
2022-03-15 03:16:24 -0400

    feat: add mv3 to next

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,4 +1,4 @@
-import type { ManifestV2 } from 'rollup-plugin-chrome-extension';
+import type { ManifestV2, ManifestV3 } from 'rollup-plugin-chrome-extension';
 const mv2: ManifestV2 = {
   name: 'Listen 1 Next',
   manifest_version: 2,
@@ -46,4 +46,49 @@ const mv2: ManifestV2 = {
   ],
   web_accessible_resources: ['assets/fonts/*', 'images/*', 'index.html']
 };
-export default mv2;
+const mv3: ManifestV3 = {
+  background: {
+    service_worker: 'serviceWorker.ts'
+  },
+  action: {
+    default_icon: { '128': 'images/logo.png' },
+    default_title: 'Listen 1'
+  },
+  description: 'One for all free music in China',
+  icons: { '128': 'images/logo.png', '16': 'images/logo_16.png', '48': 'images/logo_48.png' },
+  manifest_version: 3,
+  name: 'Listen 1',
+  permissions: ['notifications', 'unlimitedStorage', 'cookies', 'declarativeNetRequest', 'tabs'],
+  host_permissions: [
+    '*://music.163.com/*',
+    '*://*.music.163.com/*',
+    '*://*.qq.com/*',
+    '*://*.kugou.com/*',
+    '*://*.kuwo.cn/*',
+    '*://*.bilibili.com/*',
+    '*://*.bilivideo.com/*',
+    '*://*.migu.cn/*',
+    '*://*.taihe.com/*',
+    '*://api.github.com/*',
+    '*://github.com/*',
+    '*://gist.githubusercontent.com/*'
+  ],
+  declarative_net_request: {
+    rule_resources: [
+      {
+        id: 'ruleset_1',
+        enabled: true,
+        path: 'rules.json'
+      }
+    ]
+  },
+  version: '3.0.0',
+  web_accessible_resources: [{ resources: ['images/*', 'rules.json'], matches: ['<all_urls>'] }]
+  // content_scripts: [
+  //   {
+  //     matches: ['https://listen1.github.io/listen1/*'],
+  //     js: ['oauth_callback.js']
+  //   }
+  // ]
+};
+export default process.env.mv3 ? mv3 : mv2;
---
+++ src/serviceWorker.ts
@@ -0,0 +1,11 @@
+chrome.action.onClicked.addListener(() => {
+  const url = chrome.runtime.getURL('index.html');
+  chrome.tabs.query({ url }, (tabs) => {
+    console.log(tabs);
+    if (tabs.length === 0) {
+      chrome.tabs.create({ url });
+    } else {
+      chrome.tabs.update(tabs[0].id || 0, { active: true });
+    }
+  });
+});
