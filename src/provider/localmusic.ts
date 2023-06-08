39440417be1eba31a0205020e484f687a1eae2fc
Xiaokai Li <mike.lxk@gmail.com>
2023-06-08 18:51:55 +0800

    add async

--- src/provider/localmusic.ts
+++ src/provider/localmusic.ts
@@ -22,7 +22,7 @@ const provider: MusicProvider = class localmusic extends MusicResource {
         .toArray()
         .then((tracks) => (playlistInfo.order ? playlistInfo.order.map((id) => tracks.find((track) => track.id === id)) : tracks))
         .then((tracks) => {
-          tracks.map((track) => {
+          tracks.map(async(track) => {
             const md = await window.api.readTag(track.source_url);
             const imgBase64 = md.common.picture?.[0]?.data?.toString('base64');
             track.img_url = imgBase64 ? `data:${md.common.picture?.[0].format};base64,${imgBase64}` : 'images/mycover.jpg',
