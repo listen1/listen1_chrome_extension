6e1c61a596fa42e0aa0f1f5fbf8a18cd4942eaf7
scientificworld <scientificworld@126.com>
2023-10-04 11:03:10 +0800

    fix

--- js/provider/netease.js
+++ js/provider/netease.js
@@ -551,7 +551,8 @@ class netease {
           let lrc = '';
           let tlrc = '';
           if (res_data.lrc != null) {
-            lrc = res_data.lrc.lyric.replace(/(\[\d{2}:\d{2}\.\d{2}\]){2,}\n/g, '');
+            lrc = res_data.lrc.lyric.replace(/(\[\d{2}:\d{2}\.\d{2}\]){2,}\n/g, '')
+                  .replace(/(\[\d{2}:\d{2}\.\d{2}\])(\[\d{2}:\d{2}\.\d{2}\])+/g, '$1');
           }
           if (res_data.tlyric != null && res_data.tlyric.lyric != null) {
             // eslint-disable-next-line no-control-regex
